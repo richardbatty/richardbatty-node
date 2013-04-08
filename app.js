0959f7ea6419ac5bae3c15d306f86e7b38fc4b7d
Richard Batty <rjlbatty@gmail.com>
2013-04-09 00:38:39 +0100

    Replaced stylus with less, set up very basic cms

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require('express')
-  , stylus  = require('stylus')
-  , nib     = require('nib')
-  , jade    = require('jade');
+  , lessMiddleware = require('less-middleware')
+  , databaseUrl = 'mydb'
+  , collections = ['pages']
+  , db = require('mongojs').connect(databaseUrl, collections);
 
 
 var app = express();
@@ -18,20 +19,61 @@ app.configure(function () {
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
     app.use(express.logger('dev'));
-    app.use(stylus.middleware({
-        src: __dirname,
-        force: true,
-        compile: compile
+    app.use(express.bodyParser());
+    app.use(lessMiddleware({
+      src      : __dirname + "/public",
+      compress : true
     }));
     app.use(express.static(__dirname + '/public'));
 });
 
 app.get('/', function(req, res) {
-   var body = "Hello world";
    res.render('index', 
-    { title: 'Home'}
+    { title: 'Home',
+      body: 'This is the body'}
    );
 });
+
+app.get('/pages', function(req, res) {
+  res.render('pages',
+    { title: 'Pages editor' });
+})
+
+function setUrlListeners(docs) {
+  docs.forEach(function (doc) {
+    app.get(doc.path, function(req, res) {
+      res.render('index', {
+        title: doc.title,
+        body: doc.body
+      });
+    });
+  })
+};
+
+app.post('/page', function(req, res) {
+  var title = req.body.pageTitle
+    , body  = req.body.pageText
+    , path  = req.body.path
+    , reqBody = JSON.stringify(req.body);
+  console.log("Request body is: " + reqBody);
+  db.pages.save({title: title, path: path, body: body}, function(err, saved) {
+    if (err || !saved) console.log("Page not saved! Error: " + err);
+    else {
+      console.log("Page saved");
+      db.pages.find(function(err, docs) {
+        setUrlListeners(docs);
+      });
+    }
+  });
+  res.end();
+});
+
+//db.pages.remove();
+
+//db.pages.save({title: "Home", body: "Hello, this is my homepage"}, function(err, saved){
+//  if (err || !saved) console.log("Page not saved");
+//  else console.log("Page saved");
+//});
 
 app.listen(app.settings.port);
 console.log("Listening on port" + app.settings.port);
